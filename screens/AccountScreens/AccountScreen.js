3c4a42b963775cbbdf201c6ab8ef3d52fa52beb5
Elyter <eliottbreg@gmail.com>
2024-03-12 16:20:47 +0100

    Edit accountScreen

--- screens/AccountScreens/AccountScreen.js
+++ screens/AccountScreens/AccountScreen.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import { COLORS } from '../../assets/colors';
+
 const AccountScreen = () => {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Welcome to the Account Screen!</Text>
-            {/* Add your content here */}
+            <Text style={styles.title}>pp Section</Text>
         </View>
     );
 };
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: COLORS.lightblack,
     },
     title: {
         fontSize: 24,
         fontWeight: 'bold',
         marginBottom: 16,
+        color: "#FFF"
     },
 });
+
 
 export default AccountScreen;
