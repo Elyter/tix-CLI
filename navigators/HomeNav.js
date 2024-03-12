64f89603490bfc2866987195334fd5da86f29992
Elyter <eliottbreg@gmail.com>
2024-03-12 15:40:30 +0100

    add colors to assets

--- navigators/HomeNav.js
+++ navigators/HomeNav.js
@@ -7,7 +7,9 @@ const Stack = createStackNavigator();
 export default function HomeNav() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} options={{
+        headerShown: false
+      }}/>
     </Stack.Navigator>
   );
 }
