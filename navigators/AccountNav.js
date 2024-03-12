888aca7e43560b9fc9573c18f9fbab6d67ed0faf
Elyter <eliottbreg@gmail.com>
2024-03-12 18:24:06 +0100

    Add LoginScreen

--- navigators/AccountNav.js
+++ navigators/AccountNav.js
@@ -1,6 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack';
 
 import AccountScreen from '../screens/AccountScreens/AccountScreen.js';
+import LoginScreen from '../screens/AccountScreens/LoginScreen.js';
 
 const Stack = createStackNavigator();
 
@@ -8,6 +9,9 @@ export default function AccountNav() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Account" component={AccountScreen} options={{
+        headerShown: false
+      }} />
+      <Stack.Screen name="Login" component={LoginScreen} options={{
         headerShown: false
       }} />
     </Stack.Navigator>
