05ec44a06257284b86b580ed58b32be52bc4142e
Elyter <eliottbreg@gmail.com>
2024-04-10 16:19:00 +0200

    OrgaNav working

---
+++ organizer/OrganizerNav.js
@@ -0,0 +1,9 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function OrganizerNav() {
+  return (
+    <Text>Test</Text>
+  );
+}
