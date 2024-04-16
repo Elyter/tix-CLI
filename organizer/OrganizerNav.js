b002b4bfafceddced3e15cb93154dcb72ae75309
ssalim <salim.skander@ynov.com>
2024-04-16 15:07:12 +0200

    OrgAccount

--- organizer/OrganizerNav.js
+++ organizer/OrganizerNav.js
@@ -1,6 +1,18 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function OrganizerNav() {
   return (
