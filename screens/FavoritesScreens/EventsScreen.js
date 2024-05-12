7b215928bfb5851076069f4c8e7749da931d0e02
Elyter <eliottbreg@gmail.com>
2024-05-13 01:47:00 +0200

    reload likes shown

--- screens/FavoritesScreens/EventsScreen.js
+++ screens/FavoritesScreens/EventsScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, FlatList, ActivityIndicator, Touchable, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, FlatList, ActivityIndicator, Touchable, TouchableOpacity, RefreshControl } from 'react-native';
 import { COLORS } from '../../assets/colors';
 import EventCard from '../../components/EventCard';
 import axios from 'axios';
@@ -11,53 +11,67 @@ const EventsScreen = ({navigation}) => {
     const [loading, setLoading] = useState(true);
     const [userData, setUserData] = useState();
     const [events, setEvents] = useState([]);
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = () => {
+        setRefreshing(true);
+        getData();
+        setTimeout(() => {
+            setRefreshing(false);
+        }, 300);
+    };
+
 
     useEffect(() => {
         setLoading(true);
-        const getData = async () => {
-            try {
-                const value = await AsyncStorage.getItem('userData');             
-                const likesUrl = API_URL + '/likes/' + value.replace(/"/g, '');
-                axios.get(likesUrl)
-                .then((likesResponse) => {
-                    // Create an array to store promises
-                    const promises = [];
-                    likesResponse.data.forEach(element => {
-                        // Push each axios request promise to the array
-                        promises.push(
-                            axios.get(API_URL + '/events/' + element.eventId)
-                            .then((eventResponse) => {
-                                console.log('Event data:', eventResponse.data);
-                                return eventResponse.data; // Return the event data
-                            })
-                            .catch(error => {
-                                console.error('Error fetching event:', error);
-                                return null; // Return null if an error occurs
-                            })
-                        );
-                    });
-    
-                    // Wait for all promises to resolve
-                    Promise.all(promises)
-                    .then(eventData => {
-                        // Filter out null values (in case of errors)
-                        const filteredEventData = eventData.filter(data => data !== null);
-                        // Update state with filtered event data
-                        setEvents(prevEvents => [...prevEvents, ...filteredEventData]);
-                        setLoading(false);
-                    });
-                })
-                .catch((error) => {
-                    if (error.response.status === 404) {
-                        setLoading(false);
-                    }
-                });
-            } catch (error) {
-                console.error('Error getting user data:', error);
-            }
-        };
+        
         getData();
     }, []); // Empty dependency array to trigger useEffect only once
+
+    const getData = async () => {
+        setEvents([]);
+        try {
+            const value = await AsyncStorage.getItem('userData');             
+            const likesUrl = API_URL + '/likes/' + value.replace(/"/g, '');
+            axios.get(likesUrl)
+            .then((likesResponse) => {
+                // Create an array to store promises
+                const promises = [];
+                likesResponse.data.forEach(element => {
+                    // Push each axios request promise to the array
+                    promises.push(
+                        axios.get(API_URL + '/events/' + element.eventId)
+                        .then((eventResponse) => {
+                            console.log('Event data:', eventResponse.data);
+                            return eventResponse.data; // Return the event data
+                        })
+                        .catch(error => {
+                            console.error('Error fetching event:', error);
+                            return null; // Return null if an error occurs
+                        })
+                    );
+                });
+
+                // Wait for all promises to resolve
+                Promise.all(promises)
+                .then(eventData => {
+                    // Filter out null values (in case of errors)
+                    const filteredEventData = eventData.filter(data => data !== null);
+                    // Update state with filtered event data
+                    setEvents(prevEvents => [...prevEvents, ...filteredEventData]);
+                    setLoading(false);
+                });
+            })
+            .catch((error) => {
+                if (error.response.status === 404) {
+                    setLoading(false);
+                }
+            });
+        } catch (error) {
+            console.error('Error getting user data:', error);
+        }
+    };
+
     
 
     return (
@@ -69,6 +83,12 @@ const EventsScreen = ({navigation}) => {
                     <FlatList
                         data={events}
                         keyExtractor={(item) => item.id.toString()}
+                        refreshControl={
+                            <RefreshControl
+                                refreshing={refreshing}
+                                onRefresh={onRefresh}
+                                tintColor={COLORS.orange}
+                            />}
                         renderItem={({ item }) => (
                             <TouchableOpacity onPress={() => navigation.navigate("EventDetails", {id: item.id})}>
                                 <EventCard
