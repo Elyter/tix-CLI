d012aef40037a5e5add640215157aba6a00901ba
Elyter <eliottbreg@gmail.com>
2024-05-14 13:04:00 +0200

    upgrade to sdk 51 + add orga info in likes

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,15 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-  };
-};
-
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["babel-preset-expo"],
   plugins: [
-    ["module:react-native-dotenv", {
-      "envName": "APP_ENV",
-      "moduleName": "@env",
-      "path": ".env",
-      "safe": false,
-      "allowUndefined": true,
-      "verbose": false
-    }]
-  ]
-};
+    [
+      "module:react-native-dotenv",
+      {
+        moduleName: "@env",
+        path: ".env",
+      },
+    ],
+  ],
+  };
+  };
